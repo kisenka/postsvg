792adeee7c507bc0ebfdc72edb536f15de363dc8
kisenka <qtuzov@gmail.com>
2016-01-15 08:50:15 +0300

    Add Document#getId() method & tests

--- test/document.test.js
+++ test/document.test.js
@@ -3,9 +3,27 @@ var expect = require('chai').expect;
 var Result = require('../lib/result');
 var Document = require('../lib/document');
 
+function arrayHasDuplicates(array) {
+  array.sort();
+
+  var last = array[0];
+  for (var i = 1, len = array.length; i < len; i++) {
+    if (array[i] === last) return true;
+    last = array[i];
+  }
+
+  return false;
+}
+
 describe('Document', function() {
 
   var input = '<svg />';
+  var doc, $;
+
+  beforeEach(function() {
+    doc = new Document(input);
+    $ = doc.$;
+  });
 
   describe('#constructor()', function() {
     it('throws when content is not a string', function() {
@@ -15,18 +33,12 @@ describe('Document', function() {
     });
 
     it('creates Cheerio factory function', function() {
-      var doc = new Document(input);
-      var $ = doc.$;
-
       expect($).to.be.a('function');
       expect($.fn).to.equals($.prototype);
       expect($._root).to.be.an('object');
     });
 
     it('properly set parse options', function() {
-      var doc = new Document(input);
-      var $ = doc.$;
-
       expect($._options).to.eql(Document.defaultParseOptions);
     });
   });
@@ -37,17 +49,14 @@ describe('Document', function() {
     });
   });
 
-  describe('root()', function() {
+  describe('#root()', function() {
     it('returns document root node', function() {
-      var doc = new Document(input);
-
-      expect(doc.root()).to.eql(doc.$(':root'));
+      expect(doc.root()).to.eql($(':root'));
     });
   });
 
-  describe('clone()', function() {
+  describe('#clone()', function() {
     it('returns cloned document', function() {
-      var doc = new Document(input);
       var cloned = doc.clone();
 
       expect(cloned).to.be.instanceOf(Document);
@@ -56,10 +65,39 @@ describe('Document', function() {
     });
   });
 
-  it('toString()', function() {
-    var doc = new Document(input);
-
+  it('#toString()', function() {
     expect(doc.toString()).to.be.a('string');
   });
+
+  describe('#getId()', function() {
+    it('returns string', function() {
+      expect(doc.getId()).to.be.a('string');
+    });
+
+    it('returns the same id on every call', function () {
+      var id = doc.getId();
+      var alwaysReturnTheSameId = true;
+      var returnValue;
+
+      for (var i = 0; i < 1000; i++) {
+        returnValue = doc.getId();
+        if (returnValue !== id) {
+          alwaysReturnTheSameId = false;
+          break;
+        }
+      }
+
+      expect(alwaysReturnTheSameId).to.be.true;
+    });
+
+    it('returns unique ids for every new instance', function () {
+      var ids = [];
+
+      for (var i = 0; i < 1000; i++)
+        ids.push(new Document('').getId());
+
+      expect(arrayHasDuplicates(ids)).to.be.false;
+    });
+  })
 
 });
